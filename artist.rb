dbcafa809141e287660d22633de1dda82e8e0818
Theo Weller <weller.theodor@gmail.com>
2019-03-23 21:28:09 +0000

    Automatically backed up by Learn

--- artist.rb
+++ artist.rb
@@ -1,2 +1,10 @@
-class Artist < ActiveRecord::Base
+class CreateArtists < ActiveRecord::Migration
+  def change
+    create_table :artists do |t|
+      t.string :name
+      t.string :genre
+      t.integer :age
+      t.string :hometown
+    end
+  end
 end
